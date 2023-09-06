9f4e8c7a1053e283ab503e62aef1edc255255162
Kyra Polk <kyraambeau@gmail.com>
2023-09-06 14:39:02 -0500

    adding functionality to colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
+import { useState } from 'react'
+//const [selectedColor, setSelectedColor] = useState("")
+
 // Write your Color component here
 const Color = (props) => {
   //console.log(props)
   return <div className={props.color}></div>
 }
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState("")
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
       <div id="colors-list">
-        <Color color="green"/>
-        <Color color="blue"/>
-        <Color color="violet"/></div>
+        <Color color="green" setSelectedColor={setSelectedColor}/>
+        <Color color="blue" setSelectedColor={setSelectedColor}/>
+        <Color color="violet" setSelectedColor={setSelectedColor}/></div>
     </div>
   );
 };
